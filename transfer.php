b11685bf438665ae105ea73a63176412549bae09
NEELSHINDE22 <122021289+NEELSHINDE22@users.noreply.github.com>
2023-01-05 18:12:37 +0530

    Update transfer.php

--- transfer.php
+++ transfer.php
@@ -51,6 +51,7 @@
                           </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
                       </form>
-                      
+                                      <footer style="text-align: center;">&#169; 2022,NS BANK<br>Developed by Neel Shinde <br>as a part of TSF GRIP</footer>
+
                 </body>
                 </html>
